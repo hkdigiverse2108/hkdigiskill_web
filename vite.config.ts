540c39687ce8e34b058528dfcd65d2a655f6c95c
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2025-11-18 20:33:51 +0530

    Project Setup

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const PORT = Number(env.VITE_PORT);
+  const API_BASE_URL = env.VITE_API_BASE_URL;
+
+  // https://vite.dev/config/
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+    server: {
+      port: PORT,
+      proxy: {
+        "/api": {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    define: {
+      "process.env.VITE_API_BASE_URL": JSON.stringify(API_BASE_URL),
+    },
+  };
+});
