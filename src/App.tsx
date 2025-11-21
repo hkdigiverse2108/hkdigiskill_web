84f5564da48736797210ea48db1d96f3cb93fa88
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2025-11-21 11:05:35 +0530

    Basic Header Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./Routers/indx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
-  return <RouterProvider router={Router} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router} />;
+    </QueryClientProvider>
+  );
 };
 
 export default App;
